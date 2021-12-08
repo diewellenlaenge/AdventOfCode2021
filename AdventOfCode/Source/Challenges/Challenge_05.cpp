c4634d46e05b69301c7daf965e1516deed18d2f2
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2021-12-08 01:43:36 +0100

    Add (stupid) day 05 part 2

--- AdventOfCode/Source/Challenges/Challenge_05.cpp
+++ AdventOfCode/Source/Challenges/Challenge_05.cpp
@@ -28,7 +28,7 @@ EErrorCode CChallenge_05::SetUp_FirstPart()
 		return readErrorCode;
 	}
 
-	// version 1: 
+	// version 1: ranges fail
 #if FIRSTPART_SETUP_VERSION == 1
 	for (const auto& line : lines)
 	{
@@ -150,36 +150,106 @@ EErrorCode CChallenge_05::CleanUp_FirstPart()
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 EErrorCode CChallenge_05::SetUp_SecondPart()
 {
-#define SECONDPART_SETUP_VERSION 1
-
-	std::vector<std::string> lines;
-	EErrorCode const readErrorCode = FileHelper::ReadLines(sm_inputFilePath, lines);
-	if (readErrorCode != EErrorCode::Success)
-	{
-		return readErrorCode;
-	}
-
-	// version 1: 
-#if SECONDPART_SETUP_VERSION == 1
-
-#endif
-
-	return EErrorCode::NotImplemented;
+	return SetUp_FirstPart();
 }
 
 EErrorCode CChallenge_05::Run_SecondPart()
 {
 #define SECONDPART_VERSION 1
 
-	// version 1: 
+	// version 1: oh my...
 #if SECONDPART_VERSION == 1
+	std::map<int, std::map<int, std::size_t>> intersections;
 
+	for (const auto& coords : lines_)
+	{
+		// never do this at home, I'm an expert
+		const auto& line{ *reinterpret_cast<const line_type*>(coords.data()) };
+
+		if (line.p1.x == line.p2.x)
+		{
+			auto end = std::max(line.p1.y, line.p2.y) + 1;
+
+			for (auto y{ std::min(line.p1.y, line.p2.y) }; y < end; ++y)
+			{
+				intersections[line.p1.x][y]++;
+			}
+		}
+		else if (line.p1.y == line.p2.y)
+		{
+			auto end = std::max(line.p1.x, line.p2.x) + 1;
+
+			for (auto x{ std::min(line.p1.x, line.p2.x) }; x < end; ++x)
+			{
+				intersections[x][line.p1.y]++;
+			}
+		}
+		else
+		{
+			if (line.p1.x < line.p2.x)
+			{
+				if (line.p1.y < line.p2.y)
+				{
+					for (auto x{ line.p1.x }, y{ line.p1.y }; x < line.p2.x + 1 && y < line.p2.y + 1; ++x, ++y)
+					{
+						intersections[x][y]++;
+					}
+				}
+				else
+				{
+					for (auto x{ line.p1.x }, y{ line.p1.y }; x < line.p2.x + 1 && y > line.p2.y - 1; ++x, --y)
+					{
+						intersections[x][y]++;
+					}
+				}
+			}
+			else
+			{
+				if (line.p1.y < line.p2.y)
+				{
+					for (auto x{ line.p1.x }, y{ line.p1.y }; x > line.p2.x - 1 && y < line.p2.y + 1; --x, ++y)
+					{
+						intersections[x][y]++;
+					}
+				}
+				else
+				{
+					for (auto x{ line.p1.x }, y{ line.p1.y }; x > line.p2.x - 1 && y > line.p2.y - 1; --x, --y)
+					{
+						intersections[x][y]++;
+					}
+				}
+			}
+
+			// what's the problem here?
+			/*
+			auto x{std::min(line.p1.x, line.p2.x)};
+			auto y{ std::min(line.p1.y, line.p2.y) };
+			auto endX{ std::max(line.p1.x, line.p2.x) + 1 };
+			auto endY{ std::max(line.p1.y, line.p2.y) + 1 };
+
+			assert(y - x == endY - endX);
+			
+			for (; x < endX && y < endY; ++x, ++y)
+			{
+				intersections[x][y]++;
+			}*/
+		}
+	}
+
+	for (const auto& [x, ys] : intersections)
+	{
+		for (const auto& [y, count] : ys)
+		{
+			if (count > 1) ++count_;
+		}
+	}
 #endif
 
-	return EErrorCode::NotImplemented;
+	return EErrorCode::Success;
 }
 
 EErrorCode CChallenge_05::CleanUp_SecondPart()
 {
-	return EErrorCode::NotImplemented;
+	return CleanUp_FirstPart();
 }
